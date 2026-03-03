3338623819f2690afb3b565fbedadea1919663b2
ivanglushkov <glushkov1997@icloud.com>
2026-03-03 23:02:26 +0300

    feat: add app entry point

---
+++ WordOfTheDay/WordOfTheDayApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct WordOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+    }
+}
